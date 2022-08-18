fa87a382b243ad32bf5cc9774175cb53ecfe26b3
Piotr Mościcki <m.piotr@moscicki.pl>
2022-08-18 19:43:45 +0000

    Working find_same_rank

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use std::vec::Vec;
+use std:: {
+    ops::Index,
+    primitive::usize,
+    vec::Vec
+};
 
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Rank {
@@ -20,6 +26,8 @@ pub enum Rank {
 }
 
 //------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Suit {
     Club,
@@ -29,6 +37,8 @@ pub enum Suit {
 }
 
 //------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Card {
     pub m_rank: Rank,
@@ -36,11 +46,22 @@ pub struct Card {
 }
 
 //------------------------------------------------------------------------------------------------
+impl Card {
+    pub fn new(rank: Rank, suit: Suit) -> Self {
+        return Card{ m_rank: rank, m_suit: suit };
+    }
+}
+
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 #[derive(Debug, Clone)]
 pub struct Deck {
     m_cards: Vec<Card>
 }
 
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------
 impl Deck {
     pub fn new() -> Deck {
@@ -51,17 +72,27 @@ impl Deck {
 
     pub fn cards(&self) -> &Vec<Card> { return &self.m_cards; }
 
-    pub fn add(&mut self, card: &Card) {
+    pub fn len(&self) -> usize { return self.m_cards.len(); }
+
+    pub fn add(&mut self, card:&Card) {
         self.m_cards.push(*card);
     }
 }
 
+impl Index<usize> for Deck {
+    type Output = Card;
+
+    fn index(&self, idx: usize) -> &Self::Output {
+        return &self.m_cards[idx];
+    }
+}
+
 //------------------------------------------------------------------------------------------------
-pub fn find_same_rank(rank: Rank, deck: &Deck) -> Vec<std::primitive::usize> {
-    let mut indices : Vec<std::primitive::usize> = vec!();
+pub fn find_same_rank(rank: Rank, deck: &Deck) -> Vec<usize> {
+    let mut indices : Vec<usize> = vec!();
     
-    for idx in 0.. deck.cards().len() {
-        if deck.cards()[idx].m_rank == rank {
+    for idx in 0.. deck.len() {
+        if deck[idx].m_rank == rank {
             indices.push(idx);
         }
     }
@@ -85,8 +116,42 @@ mod tests {
     #[test]
     fn find_four_of_rank() {
         let mut hand = Deck::new();
-        hand.add( &Card { m_rank: Rank::Ace, m_suit: Suit::Spade } );
-        assert_eq!(4, 4);
+        hand.add(&Card::new(Rank::Ace, Suit::Spade));
+        hand.add(&Card::new(Rank::Ace, Suit::Club));
+        hand.add(&Card::new(Rank::Ace, Suit::Diamond));
+        hand.add(&Card::new(Rank::Ace, Suit::Heart));
+        hand.add(&Card::new(Rank::Queen, Suit::Spade));
+        hand.add(&Card::new(Rank::Queen, Suit::Club));
+        hand.add(&Card::new(Rank::Queen, Suit::Diamond));
+        hand.add(&Card::new(Rank::King, Suit::Heart));
+        hand.add(&Card::new(Rank::King, Suit::Spade));
+        hand.add(&Card::new(Rank::Jack, Suit::Club));
+        hand.add(&Card::new(Rank::Ten, Suit::Diamond));
+        hand.add(&Card::new(Rank::Nine, Suit::Heart));
+        hand.add(&Card::new(Rank::Seven, Suit::Spade));
+        hand.add(&Card::new(Rank::Four, Suit::Club));
+
+        let aces = find_same_rank(Rank::Ace, &hand);
+        assert_eq!(aces.len(), 4);
+        assert_eq!(aces[0], 0);
+        assert_eq!(aces[1], 1);
+        assert_eq!(aces[2], 2);
+        assert_eq!(aces[3], 3);
+
+        let queens = find_same_rank(Rank::Queen, &hand);
+        assert_eq!(queens.len(), 3);
+        assert_eq!(queens[0], 4);
+        assert_eq!(queens[1], 5);
+        assert_eq!(queens[2], 6);
+
+        let kings = find_same_rank(Rank::King, &hand);
+        assert_eq!(kings.len(), 2);
+        assert_eq!(kings[0], 7);
+        assert_eq!(kings[1], 8);
+
+        let jacks = find_same_rank(Rank::Jack, &hand);
+        assert_eq!(jacks.len(), 1);
+        assert_eq!(jacks[0], 9);
     }
 }
 
